97c982f5e4fd96ff2d10bac34ba6e33fc3979b03
amabdelftah <ahmedabdelftah95165@gmail.com>
2022-04-16 11:49:37 +0200

    Add route names

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
         <div class="text-center">
-            <button type="button" class="mt-4 btn btn-success">Create Post</button>
+            <a href="{{ route('posts.create') }}" class="mt-4 btn btn-success">Create Post</a>
         </div>
         <table class="table mt-4">
             <thead>
@@ -24,7 +24,7 @@
                 <td>{{ $post['post_creator'] }}</td>
                 <td>{{ $post['created_at'] }}</td>
                 <td>
-                    <a href="#" class="btn btn-info">View</a>
+                    <a href="{{ route('posts.show', ['post' => $post['id']]) }}" class="btn btn-info">View</a>
                     <a href="#" class="btn btn-primary">Edit</a>
                     <a href="#" class="btn btn-danger">Delete</a>
                 </td>
